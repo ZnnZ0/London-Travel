4efe80b022e88c6eadc4e40165bbe135eaa552c7
Michiaki Mese <pgpp000@gmail.com>
2023-03-16 15:09:17 +0100

    last update

--- js/booking.js
+++
@@ -1,7 +0,0 @@
-function getSelectedValue_1day() {
-  var optionValue = document.getElementById("mySelect_1day").value;
-  var memberValue = document.getElementById("member_1day").value;
-  const selectedDate = document.getElementById("date_1day").value;
-  const email = document.getElementById("email1").value;
-  console.log(optionValue + ":" + memberValue + ":" + selectedDate + email);
-}
