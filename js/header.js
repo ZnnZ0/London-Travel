f905ebc38c19c32d563d67c19839e4c4b509995b
Michiaki Mese <pgpp000@gmail.com>
2023-03-21 09:19:00 +0100

    debug

--- js/header.js
+++ js/header.js
@@ -1,13 +1,13 @@
 window.addEventListener("load", (event) => {
-    var nodeList = document.querySelectorAll(".has-sub-menu"); //プルダウンメニューのNodeList
+    var nodeList = document.querySelectorAll(".has-sub-menu");
 
     nodeList.forEach(function (children) {
         children.addEventListener("mouseover", function () {
-            children.querySelector(".sub-menu").classList.add("sub-menu-active"); //オンマウス時にsub-list-activeクラスを付与
+            children.querySelector(".sub-menu").classList.add("sub-menu-active");
         });
 
         children.addEventListener("mouseout", function () {
-            children.querySelector(".sub-menu").classList.remove("sub-menu-active"); //アウトマウス時にsub-list-activeクラスを削除
+            children.querySelector(".sub-menu").classList.remove("sub-menu-active");
         });
     });
 });
